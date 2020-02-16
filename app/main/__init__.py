e152cebd2ef6934288ba7459c4c7a2bb6b63253d
Allan <allanlumumba@gmail.com>
2020-02-16 18:12:11 +0300

    create pitch form and comment form

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask_bootstrap import Bootstrap
 main = Blueprint('main',__name__)
 from . import views,errors
 
--- app/main/forms.py
+++ app/main/forms.py
@@ -7,4 +7,20 @@ from wtforms import ValidationError
 class PitchForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
     description = TextAreaField("What's your pitch?", validators=[Required()])
-    category = RadioField('Label', )
+    category = RadioField('Label', choices=[('promotionpitch','promotionpitch'),('interviewpitch', 'interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')], validators=[Required()])
+    submit = SubmitField('submit')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Add a comment..')
+    submit = SubmitField('Submit')
+
+
+class UpvoteForm(FlaskForm):
+    submit = SubmitField()
+
+
+class DownvoteForm(FlaskForm):
+    submit = SubmitField()
+
+
