3ad9f8b34e7cae9601b92eec9c3481a6da5afb8f
Allan <allanlumumba@gmail.com>
2020-02-16 17:52:32 +0300

    create secret key

--- config.py
+++ config.py
@@ -1,1 +1,31 @@
 
+import os
+
+
+class Config:
+    """"""
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://allan:852456@localhost/pitch'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+   # simple mde configurations
+   SIMPLEMDE_JS_IIFE = True
+   SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    """"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    DEBUG = True
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://allan:852456@localhost/pitch'
+    """"""
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
