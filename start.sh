841a9f63f97af976e06c9b1c3ef242992a5f60f4
Allan <allanlumumba@gmail.com>
2020-02-21 13:47:36 +0300

    style index page

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 export SECRET_KEY=0f5cb998cf6c2d1510ece66c3c0d0f92
-export MAIL_USERNAME = 'allanlumumba@gmail.com'
-export MAIL_PASSWORD = 'L@$$$100'
+export DATABASE_URL='postgres+psycopg2://allan:1234@localhost/pitch'
+export MAIL_USERNAME='allanlumumba@gmail.com'
+export MAIL_PASSWORD='L@$$$100'
 
 
 python3.7 manage.py server
